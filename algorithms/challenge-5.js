1f4a09556f7f6c82624db468896c359056720a4c
Ndefo Tochukwu <42077361+TCipher@users.noreply.github.com>
2022-06-22 15:23:46 +0100

    Update challenge-5.js

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,9 +1,6 @@
 function filterList(arr) {
-<<<<<<< HEAD
  return  arr.filter( arr => typeof arr === 'number')
-=======
- return arr.filter( arr => typeof arr === 'number')
->>>>>>> 045b53413ccfefb3e2ef550057d816eae00e77be
+
  
  }
 
